215ea1ee471008c776d6f89488afbaa95dec2351
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-04-16 20:04:48 -0300

    :hammer: Refactor app init and reload functionalities

--- web/js/App.js
+++ web/js/App.js
@@ -1,30 +1,30 @@
-import { handleForm } from './modules/handleCharCreationForm.js'
+import { charCreationForm } from './modules/formAndModalHandlers.js';
 import { Storage } from './modules/handleStorage.js'
 import { handleChar } from './modules/handleChar.js'
 import { Sheet } from './modules/handleHtmlSheet.js'
-import { watchClick } from './modules/handleActionButtons.js'
+import { watchPlayerAction } from './modules/handleActionButtons.js'
 import { toggleModal } from './modules/handleModals.js'
 
 export const App = {
   init() {
     Storage.set(handleChar.char)
-    App.checkChar()
+
+    if (handleChar.char == null) {
+      toggleModal(0, 'show')
+      charCreationForm.watchForm()
+      return
+    }
+
+    toggleModal(0, 'hide')
+    Sheet.initTable()
+    watchPlayerAction()
+    return
   },
 
   reload() {
     Sheet.clearTables()
-    App.init()
-  },
 
-  checkChar() {
-    if (handleChar.char == null) {
-      toggleModal(0, 'show')
-      handleForm.watchForm()
-    } else {
-      toggleModal(0, 'hide')
-      Sheet.initTable()
-      watchClick()
-    }
+    App.init()
   }
 }
 
