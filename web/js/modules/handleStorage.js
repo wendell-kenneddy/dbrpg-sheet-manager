158d92a0772c9a8c6ed01ce959ed2ac03d9c1cf4
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-04-16 20:09:35 -0300

    :sparkles: Add missing return to set function

--- web/js/modules/handleStorage.js
+++ web/js/modules/handleStorage.js
@@ -1,11 +1,10 @@
-const Storage = {
+export const Storage = {
   get() {
     return JSON.parse(localStorage.getItem('rpg-marcao: char')) || null
   },
 
   set(char) {
     localStorage.setItem('rpg-marcao: char', JSON.stringify(char))
+    return
   }
 }
-
-export { Storage }
