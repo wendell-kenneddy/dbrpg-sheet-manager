c7cec9122fbbc319f82a61ffdf844f04788e6b5e
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-04-16 20:10:38 -0300

    :sparkles: Add missing ; and fix general typo

--- web/js/modules/handleToast.js
+++ web/js/modules/handleToast.js
@@ -1,19 +1,21 @@
 export const Toast = {
   open(msg) {
-    const toast = document.getElementById('warning-toast')
-    const dismissBtn = document.querySelector('#warning-toast button')
+    const toast = document.getElementById('warning-toast');
+    const dismissBtn = document.querySelector('#warning-toast button');
 
-    toast.firstElementChild.innerHTML = msg
+    toast.firstElementChild.innerHTML = msg;
     toast.classList.remove('hide')
     toast.classList.add('fadein')
-    dismissBtn.addEventListener('click', this.close)
+    dismissBtn.addEventListener('click', Toast.close)
+    return
   },
 
   close() {
-    const toast = document.getElementById('warning-toast')
+    const toast = document.getElementById('warning-toast');
 
     toast.classList.add('hide')
     toast.classList.remove('fadein')
-    toast.removeEventListener('click', this.close)
+    toast.removeEventListener('click', Toast.close)
+    return
   }
 }
