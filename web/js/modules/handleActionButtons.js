75e105e09c0c129480f79c584ec2de9481fe335c
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-04-16 20:07:07 -0300

    :hammer: Update action handlers

--- web/js/modules/handleActionButtons.js
+++ web/js/modules/handleActionButtons.js
@@ -1,85 +1,61 @@
 import { toggleModal } from './handleModals.js'
-import { handleChar } from './handleChar.js'
-import { attributeForm } from './handleAttributesModify.js'
-import { rollForm } from './handleAttributeRollForm.js'
-import { addItemForm } from './handleItemAddItemForm.js'
-import { removeItemPrompt } from './handleItemRemove.js'
-import { statusModifyForm } from './handleStatusModifyForm.js'
-import { arbitraryAttributesForm } from './handleArbitraryAttributesForm.js';
-import { timeChamber } from './handleTimeChamber.js'
-import { handleCharacteristicRemove } from './handleCharacteristicRemove.js'
+import { arbitraryAttributesModifyForm, attributeRollForm, primaryAttributesUpgradeForm, statusModifyForm, addItemForm, removeItemModal, timeChamberModal, characteristicRemoveModal } from './formAndModalHandlers.js';
+import { charDeleteModal } from './formAndModalHandlers.js'
 
-export const watchDelete = e => {
-  const btnConfirm = document.getElementById('confirm-char-delete')
-  const btnCancel = document.getElementById('cancel-char-delete')
-
-  if (e.target == btnConfirm) {
-    toggleModal(2, 'hide')
-    handleChar.deleteChar()
-    return
-  }
-
-  if (e.target == btnCancel) {
-    toggleModal(2, 'hide')
-    document.getElementById('dialogue-box').removeEventListener('click', watchDelete)
-    return
-  }
-}
-
-export const watchClick = () => {
+export const watchPlayerAction = () => {
   document.getElementById('player-actions').addEventListener('click', e => {
 
-    if (e.target == document.getElementById('char-attributes-modify')) {
-      toggleModal(1, 'show')
-      attributeForm.watchModify()
-      return
-    }
-
     if (e.target == document.getElementById('char-delete')) {
-      toggleModal(2, 'show')
-      document.getElementById('dialogue-box').addEventListener('click', watchDelete)
+      toggleModal(1, 'show')
+      charDeleteModal.watchDelete()
       return
     }
 
     if (e.target == document.getElementById('char-roll-test')) {
+      toggleModal(2, 'show')
+      attributeRollForm.watchRoll()
+      return
+    }
+
+    if (e.target == document.getElementById('char-attributes-modify')) {
       toggleModal(3, 'show')
-      rollForm.watchRoll()
+      primaryAttributesUpgradeForm.watchPrimaryAttributeUpgrade()
+      return
+    }
+
+    if (e.target == document.getElementById('char-arbitrary-attributes')) {
+      toggleModal(4, 'show')
+      arbitraryAttributesModifyForm.watchArbitraryAttributeModify()
+      return
+    }
+
+    if (e.target == document.getElementById('char-status-modify')) {
+      toggleModal(5, 'show')
+      statusModifyForm.watchStatusModify()
       return
     }
 
     if (e.target == document.getElementById('char-add-item')) {
-      toggleModal(4, 'show')
+      toggleModal(6, 'show')
       addItemForm.watchItemAdd()
       return
     }
 
     if (e.target == document.getElementById('char-remove-item')) {
-      toggleModal(5, 'show')
-      removeItemPrompt.watchRemove()
-      return
-    }
-
-    if (e.target == document.getElementById('char-status-modify')) {
-      toggleModal(6, 'show')
-      statusModifyForm.watchStatusModify()
-      return
-    }
-
-    if (e.target == document.getElementById('char-arbitrary-attributes')) {
       toggleModal(7, 'show')
-      arbitraryAttributesForm.watchArbitraryModify()
+      removeItemModal.watchItemRemove()
       return
     }
 
     if (e.target == document.getElementById('open-time-chamber')) {
       toggleModal(8, 'show')
-      timeChamber.watchCharacteristics()
+      timeChamberModal.watchCharacteristics()
       return
     }
 
     if (e.target == document.getElementById('char-remove-characteristic')) {
-      toggleModal(11, 'show')
-      handleCharacteristicRemove.watchCharacteristicRemove()
+      toggleModal(9, 'show')
+      characteristicRemoveModal.watchCharacteristicRemove()
       return
     }
   })
